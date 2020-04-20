0b5d5d04c91a36c52234b1ab45e28a3be2bdbc8a
Aleixo G Junior <aleixo.cco@gmail.com>
2020-04-20 14:34:57 -0300

    Implemented Dashboard template and styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/23524004?s=460&u=5a073f4ffab6f91fff38530275b1554fdd6d7302&v=4"
+            alt="Aleixo Guarizo"
+          />
+          <div>
+            <strong>aleixogjunior/AirCnc</strong>
+            <p>Detalhes</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
